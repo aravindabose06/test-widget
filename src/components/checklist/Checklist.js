37105fae49c5f95a465f5b5b5da539994232b7a4
Aravind <aravinda@gox.ai>
2025-09-06 22:38:50 +0530

    tested custom element with r2wc

---
+++ src/components/checklist/Checklist.js
@@ -0,0 +1,9 @@
+function Checklist() {
+  return (
+    <div className="">
+      Testing web components
+    </div>
+  );
+}
+
+export default Checklist;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import r2wc from "@r2wc/react-to-web-component"
+import Checklist from "./components/checklist/Checklist"
+
+const wcChecklist = r2wc(Checklist, {
+  props: {
+    items: "json"
+  }
+})
+
+customElements.define("r2w-checklist", wcChecklist)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
